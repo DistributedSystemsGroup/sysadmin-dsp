49a3d9c8582dd98408f21b4a2fe0121c3b0f24d4
Daniele Venzano <venza@brownhat.org>
2017-10-30 16:12:21 +0100

    Tell wget to be quiet

--- m1/cron/update_platform_image_from_grafana.sh
+++ m1/cron/update_platform_image_from_grafana.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget 'http://bigfoot-m2.eurecom.fr/grafana/render/dashboard-solo/db/platform-overview?refresh=1m&orgId=1&panelId=4&width=500&height=300' -O /var/www/html/data/media/platform-overview.png
+wget --quiet 'http://bigfoot-m2.eurecom.fr/grafana/render/dashboard-solo/db/platform-overview?refresh=1m&orgId=1&panelId=4&width=500&height=300' -O /var/www/html/data/media/platform-overview.png
 
