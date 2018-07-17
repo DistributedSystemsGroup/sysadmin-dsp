5b6edef788906fa092a67073b74bcc72625fbeee
Daniele Venzano <venza@brownhat.org>
2018-07-17 15:50:59 +0200

    Update with now grafana public endpoint

--- m1/cron/update_platform_image_from_grafana.sh
+++ m1/cron/update_platform_image_from_grafana.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget --quiet 'http://bigfoot-m2.eurecom.fr/grafana/render/dashboard-solo/db/platform-overview?refresh=1m&orgId=1&panelId=4&width=500&height=300' -O /var/www/html/data/media/platform-overview.png
+wget --no-check-certificate --quiet 'https://cloud-platform.eurecom.fr/grafana/render/dashboard-solo/db/platform-overview?refresh=1m&orgId=1&panelId=4&width=500&height=300' -O /var/www/html/data/media/platform-overview.png
 
