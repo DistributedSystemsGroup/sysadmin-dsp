06034912cc1ebde48105de98e81704e71f1f7821
Daniele Venzano <venza@brownhat.org>
2018-11-20 12:36:42 +0100

    Update the goaccess cron to get logs from traefik too

--- m1/cron/goaccess_stats.sh
+++ m1/cron/goaccess_stats.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zcat /var/log/apache2/access.log*.gz | goaccess /var/log/apache2/access.log /var/log/apache2/access.log.1 -o /var/www/html/goaccess/index.html --no-progress -
+zcat /var/log/apache2/access_traefik.log*.gz | goaccess /var/log/apache2/access_traefik.log /var/log/apache2/access_traefik.log.1 -o /var/www/html/goaccess/index.html --no-progress -
 
